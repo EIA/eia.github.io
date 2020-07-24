584bac451ec8204376716fe6f3ece12ca010b288
Eia mac 13 <angelliya00@gmail.com>
2020-07-24 23:15:59 +0800

    demo update

---
+++ ironman/2021/demo/2020_07_24/lv3.js
@@ -0,0 +1,33 @@
+const app = new PIXI.Application({ backgroundColor: 0x1099bb });
+document.body.appendChild(app.view);
+
+const bunnyLeft = PIXI.Sprite.from('assets/basics/bunny.png');
+bunnyLeft.anchor.set(0.5);
+bunnyLeft.x = app.screen.width / 2 - 100;
+bunnyLeft.y = app.screen.height / 2;
+
+const bunnyRight = PIXI.Sprite.from('assets/basics/bunny.png');
+bunnyRight.pivot.set(13, 18.5);
+bunnyRight.x = app.screen.width / 2 + 100;
+bunnyRight.y = app.screen.height / 2;
+
+app.stage.addChild(bunnyLeft);
+app.stage.addChild(bunnyRight);
+
+app.ticker.add((delta) => {
+    bunnyLeft.rotation += 0.1 * delta;
+    bunnyRight.rotation += 0.1 * delta;
+});
+
+const grid = new PIXI.Graphics();
+grid.lineStyle(1, 0x0);
+grid.moveTo(app.screen.width / 2 - 100, app.screen.height / 2);
+grid.lineTo(app.screen.width / 2 + 100, app.screen.height / 2);
+
+grid.moveTo(app.screen.width / 2 - 100, app.screen.height / 2 - 50);
+grid.lineTo(app.screen.width / 2 - 100, app.screen.height / 2 + 50);
+
+grid.moveTo(app.screen.width / 2 + 100, app.screen.height / 2 - 50);
+grid.lineTo(app.screen.width / 2 + 100, app.screen.height / 2 + 50);
+
+app.stage.addChild(grid);
