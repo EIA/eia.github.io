5caebf848904af2c22dabcaad3e07939c948b644
Eia mac 13 <angelliya00@gmail.com>
2020-08-04 22:41:14 +0800

    demo update

---
+++ ironman/2021/demo/2020_08_04-2/lv4.js
@@ -0,0 +1,29 @@
+const app = new PIXI.Application({ width:100, height: 100, backgroundColor: 0x1099bb });
+document.body.appendChild(app.view);
+
+const bunny = PIXI.Sprite.from('assets/basics/bunny.png');
+
+// bunny.anchor.set(0.5);
+bunny.pivot.set(13, 18.5);
+
+bunny.x = app.screen.width / 2;
+bunny.y = app.screen.height / 2;
+
+app.stage.addChild(bunny);
+
+bunny.buttonMode = true;
+bunny.interactive = true;
+
+bunny.hitArea = new PIXI.Rectangle(0, 0, 10, 10);
+
+bunny.on("pointerdown", ()=>{
+	console.log("pointerdown");
+});
+
+const line = new PIXI.Graphics();
+line.lineStyle(1, 0xdddddd);
+line.moveTo(app.screen.width * .5, 0);
+line.lineTo(app.screen.width * .5, app.screen.height);
+line.moveTo(0, app.screen.height * .5);
+line.lineTo(app.screen.width, app.screen.height * .5);
+app.stage.addChild(line);
