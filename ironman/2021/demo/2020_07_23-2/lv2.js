6706a753ae1879c06327a297599631dd2cc1c6b9
Eia mac 13 <angelliya00@gmail.com>
2020-09-02 14:04:22 +0800

    demo update

--- ironman/2021/demo/2020_07_23-2/lv2.js
+++ ironman/2021/demo/2020_07_23-2/lv2.js
@@ -3,9 +3,11 @@ document.body.appendChild(app.view);
 
 
 const container = new PIXI.Container();
+container.name = "container";
 app.stage.addChild(container);
 
 const bunny = PIXI.Sprite.from('assets/basics/bunny.png');
+bunny.name = "bunny";
 container.addChild(bunny);
 
 setTimeout(function(){
