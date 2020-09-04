341db485c9481947278793773e8a335c114cdfa6
Eia mac 13 <angelliya00@gmail.com>
2020-09-04 23:19:21 +0800

    demo update

--- ironman/2021/demo/2020_07_28/lv4.js
+++ ironman/2021/demo/2020_07_28/lv4.js
@@ -5,7 +5,7 @@ function createBox(color, w, h, name){
 	const boxContainer = new PIXI.Container();
 	boxContainer.name = name;
 	const box = new PIXI.Graphics();
-	box.beginFill(color, 0.7);
+	box.beginFill(color);
 	box.drawRect(0, 0, 100, 100);
 	box.endFill();
 
@@ -26,11 +26,11 @@ const box1 = createBox(0xff0000, 100, 100, 'box1');
 const box2 = createBox(0xff9900, 100, 100, 'box2');
 const box3 = createBox(0xff0000, 100, 100, 'box3');
 box1.x = 0;
-box1.y = 0;
+box1.y = 30;
 box2.x = 50;
-box2.y = 70;
+box2.y = 100;
 box3.x = 0;
-box3.y = 140;
+box3.y = 170;
 
 const textStyle = new PIXI.TextStyle({
 	fontSize: 36,
@@ -45,5 +45,7 @@ textAsContainer.addChild(box3);
 app.stage.addChild(textAsContainer);
 textAsContainer.x = 50;
 textAsContainer.y = 50;
+
+textAsContainer.setChildIndex(box3, 0);
 
 console.log("textAsContainer.children.length: ", textAsContainer.children.length);
