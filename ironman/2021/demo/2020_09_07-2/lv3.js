cfbf072f6d162eb1ce5acaf1850c2aaa65d5db18
Eia mac 13 <angelliya00@gmail.com>
2020-09-07 19:47:33 +0800

    demo update

---
+++ ironman/2021/demo/2020_09_07-2/lv3.js
@@ -0,0 +1,34 @@
+const app = new PIXI.Application({ width: 375, height:667, backgroundColor: 0x1099bb });
+document.body.appendChild(app.view);
+
+
+console.log("PIXI.utils.isMobile.apple.device: ", PIXI.utils.isMobile.apple.device);
+
+const bunny = PIXI.Sprite.from('assets/basics/bunny.png');
+
+bunny.anchor.set(0.5);
+
+
+bunny.x = app.screen.width / 2;
+bunny.y = app.screen.height / 2;
+
+app.stage.addChild(bunny);
+
+bunny.interactive = true;
+
+bunny.on("pointerdown", ()=>{
+	// console.log('pd');
+});
+
+bunny.on("pointerup", ()=>{
+	// console.log('pu');
+});
+
+
+bunny.on("click", ()=>{
+	// console.log('ck');
+});
+
+bunny.on("pointermove", ()=>{
+	console.log('pv');
+});
