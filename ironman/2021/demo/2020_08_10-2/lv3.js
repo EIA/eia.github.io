c9e3d0923226a65f1eb2038337723345cbd7c74c
Eia mac 13 <angelliya00@gmail.com>
2020-09-19 12:43:08 +0800

    demo update

--- ironman/2021/demo/2020_08_10-2/lv3.js
+++ ironman/2021/demo/2020_08_10-2/lv3.js
@@ -2,17 +2,16 @@ const app = new PIXI.Application({ backgroundColor: 0x1099bb });
 document.body.appendChild(app.view);
 
 
-let text = `test 測試`;
 const style1 = new PIXI.TextStyle({
 	fontSize: 24
 });
 
-const testText1 = new PIXI.Text(text, style1);
+const testText1 = new PIXI.Text('test 測試: 1', style1);
 testText1.x = 50;
 testText1.y = 50;
 app.stage.addChild(testText1);
 
-const testText2 = new PIXI.Text(text, style1);
+const testText2 = new PIXI.Text('test 測試: 2', style1);
 testText2.x = 50;
 testText2.y = 150;
 testText2.scale.x = 2;
@@ -23,7 +22,7 @@ app.stage.addChild(testText2);
 const style2 = new PIXI.TextStyle({
 	fontSize: 48
 });
-const testText3 = new PIXI.Text(text, style2);
+const testText3 = new PIXI.Text('test 測試: 3', style2);
 testText3.x = 50;
 testText3.y = 250;
 app.stage.addChild(testText3);
