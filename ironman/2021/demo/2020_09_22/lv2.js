2eb70600700e73ae2710996c2566ca678f760047
Eia mac 13 <angelliya00@gmail.com>
2020-09-22 01:35:18 +0800

    demo update

--- ironman/2021/demo/2020_09_22/lv2.js
+++ ironman/2021/demo/2020_09_22/lv2.js
@@ -9,4 +9,19 @@ bunny.y = app.screen.height / 2;
 app.stage.addChild(bunny);
 
 const gui = new dat.GUI();
-gui.add(bunny, 'alpha', 0, 1, 0.01).name("兔子透明度")
+gui.add(bunny, 'alpha', 0, 1, 0.01).name("兔子透明度");
+
+
+window.onresize = function (event){
+  var w = window.innerWidth;
+  var h = window.innerHeight;
+
+  app.view.style.width = w + "px";
+  app.view.style.height = h + "px";
+  app.renderer.resize(w,h);
+
+  bunny.x = app.screen.width / 2;
+  bunny.y = app.screen.height / 2;
+};
+
+onresize();
