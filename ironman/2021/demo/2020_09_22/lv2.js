78d57d024960460fd0c0ad43762c2526a6715148
Eia mac 13 <angelliya00@gmail.com>
2020-09-22 01:29:40 +0800

    demo update

---
+++ ironman/2021/demo/2020_09_22/lv2.js
@@ -0,0 +1,12 @@
+const app = new PIXI.Application({ backgroundColor: 0x1099bb });
+document.body.appendChild(app.view);
+
+const bunny = PIXI.Sprite.from('assets/basics/bunny.png');
+bunny.anchor.set(0.5);
+bunny.x = app.screen.width / 2;
+bunny.y = app.screen.height / 2;
+
+app.stage.addChild(bunny);
+
+const gui = new dat.GUI();
+gui.add(bunny, 'alpha', 0, 1, 0.01).name("兔子透明度")
