0c71b0172b4b11e32cb26c143d4a75b9f15ceb87
Eia mac 13 <angelliya00@gmail.com>
2020-09-22 12:58:38 +0800

    demo update

---
+++ ironman/2021/demo/2020_09_22/lv4-2.js
@@ -0,0 +1,105 @@
+const app = new PIXI.Application({ backgroundColor: 0x1099bb });
+document.body.appendChild(app.view);
+
+// create a texture from an image path
+const texture = PIXI.Texture.from('assets/basics/bunny.png');
+
+// Scale mode for pixelation
+texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
+
+const bunny = createBunny(
+    Math.floor(Math.random() * app.screen.width),
+    Math.floor(Math.random() * app.screen.height),
+);
+
+function createBunny(x, y) {
+    // create our little bunny friend..
+    const bunny = new PIXI.Sprite(texture);
+
+    // enable the bunny to be interactive... this will allow it to respond to mouse and touch events
+    bunny.interactive = true;
+
+    // this button mode will mean the hand cursor appears when you roll over the bunny with your mouse
+    bunny.buttonMode = true;
+
+    // center the bunny's anchor point
+    bunny.anchor.set(0.5);
+
+    // make it a bit bigger, so it's easier to grab
+    bunny.scale.set(3);
+
+    // setup events for mouse + touch using
+    // the pointer events
+    bunny
+        .on('pointerdown', onDragStart)
+        .on('pointerup', onDragEnd)
+        .on('pointerupoutside', onDragEnd)
+        .on('pointermove', onDragMove);
+
+    // For mouse-only events
+    // .on('mousedown', onDragStart)
+    // .on('mouseup', onDragEnd)
+    // .on('mouseupoutside', onDragEnd)
+    // .on('mousemove', onDragMove);
+
+    // For touch-only events
+    // .on('touchstart', onDragStart)
+    // .on('touchend', onDragEnd)
+    // .on('touchendoutside', onDragEnd)
+    // .on('touchmove', onDragMove);
+
+    // move the sprite to its designated position
+    bunny.x = x;
+    bunny.y = y;
+
+    // add it to the stage
+    app.stage.addChild(bunny);
+
+    return bunny;
+}
+
+function onDragStart(event) {
+    // store a reference to the data
+    // the reason for this is because of multitouch
+    // we want to track the movement of this particular touch
+    this.data = event.data;
+    this.alpha = 0.5;
+    this.dragging = true;
+}
+
+function onDragEnd() {
+    this.alpha = 1;
+    this.dragging = false;
+    // set the interaction data to null
+    this.data = null;
+}
+
+function onDragMove() {
+    if (this.dragging) {
+        const newPosition = this.data.getLocalPosition(this.parent);
+        this.x = newPosition.x;
+        this.y = newPosition.y;
+    }
+}
+
+
+window.onresize = function (event){
+  var w = window.innerWidth;
+  var h = window.innerHeight;
+
+  app.view.style.width = w + "px";
+  app.view.style.height = h + "px";
+  app.renderer.resize(w,h);
+
+  bunny.x = app.screen.width / 2;
+  bunny.y = app.screen.height / 2;
+};
+
+onresize();
+
+
+const gui = new dat.GUI();
+gui.remember(bunny);
+
+gui.add(bunny, 'x', -300, 300, 1).listen();
+gui.add(bunny, 'y', -300, 300, 1).listen();
