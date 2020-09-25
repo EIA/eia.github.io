90545606539165e278ae00cbd5fcc09e88a632e8
Eia mac 13 <angelliya00@gmail.com>
2020-09-25 21:50:24 +0800

    demo update

---
+++ ironman/2021/demo/2020_08_26/lv4_2_3.js
@@ -0,0 +1,45 @@
+const app = new PIXI.Application({ backgroundColor: 0x1099bb });
+document.body.appendChild(app.view);
+
+let animatedSprite;
+
+// const loader = PIXI.Loader.shared;
+const loader = new PIXI.Loader();
+// loader.add('bunny', 'assets/basics/bunny.png');
+loader.add(
+  {
+    name: 'bunny',
+    url: 'assets/basics/bunny.png',
+    onComplete:function(resources){console.log("bunny onComplete", resources)}
+  });
+// loader.add('bunniesJSON', 'bunnies.json');
+
+loader.add(
+  {
+    name: 'bunniesJSON',
+    url: 'bunnies.json',
+    onComplete:function(resources){console.log("bunniesJSON onComplete", resources)}
+  });
+
+loader.load((loader, resources) => {
+
+    console.log("loader: ", loader);
+    console.log("resources: ", resources);
+    console.log("resources.bunniesJSON: ", resources.bunniesJSON);
+    console.log("resources.bunniesJSON.textures: ", resources.bunniesJSON.textures);
+    console.log("resources.bunniesJSON.spritesheet: ", resources.bunniesJSON.spritesheet);
+
+    console.log("PIXI.utils.TextureCache: ", PIXI.utils.TextureCache);
+    console.log("PIXI.utils.BaseTextureCache: ", PIXI.utils.BaseTextureCache);
+
+
+	// let sheet = PIXI.Loader.shared.resources["assets/spritesheet.json"].spritesheet;
+  	// animatedSprite = new PIXI.AnimatedSprite(sheet.animations["image_sequence"]);
+
+  	animatedSprite = new PIXI.AnimatedSprite(resources.bunniesJSON.spritesheet.animations["bunny"]);
+    animatedSprite.animationSpeed = 0.05;
+  	app.stage.addChild(animatedSprite);
+
+  	animatedSprite.play();
+
+});
