08cfc44634464b265e20c7f2bb240dd41894a8a8
Eia mac 13 <angelliya00@gmail.com>
2020-09-27 21:17:07 +0800

    demo update

---
+++ ironman/2021/demo/2020_09_27/lv3.js
@@ -0,0 +1,12 @@
+const app = new PIXI.Application({ backgroundColor: 0x1099bb });
+document.body.appendChild(app.view);
+
+const loader = PIXI.Loader.shared;
+loader.add('bunny', 'assets/basics/bunny.png');
+loader.add('bunny', 'assets/basics/bunny2.png');
+
+loader.load((loader, resources) => {
+    // console.log("loader: ", loader, " resources: ", resources);
+    console.log('complete');
+});
+
