ead2f8658b61188e3fbf88c1cd391a056492b9b0
Eia mac 13 <angelliya00@gmail.com>
2020-09-28 14:21:31 +0800

    demo update

---
+++ ironman/2021/demo/2020_09_28-2/lv3.js
@@ -0,0 +1,39 @@
+const app = new PIXI.Application({ width: 300, height:300, backgroundColor: 0x1099bb });
+document.body.appendChild(app.view);
+
+
+const resolution = PIXI.settings.RESOLUTION;
+const filterResolution = PIXI.settings.FILTER_RESOLUTION;
+const rendererResolution = app.renderer.resolution;
+
+console.log('resolution: ', resolution);
+console.log('filterResolution: ', filterResolution);
+console.log('rendererResolution: ', rendererResolution);
+
+
+const testPic = PIXI.Sprite.from('test.png');
+
+testPic.anchor.set(0.5);
+testPic.x = app.screen.width / 2;
+testPic.y = app.screen.height / 2;
+
+app.stage.addChild(testPic);
+
+/*
+window.onresize = function (event) {
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+
+    app.view.style.width = w + "px";
+    app.view.style.height = h + "px";
+    app.renderer.resize(w, h);
+
+    testPic.x = app.screen.width / 2;
+    testPic.y = app.screen.height / 2;
+
+};
+
+onresize();
+*/
+
+alert("1421");
