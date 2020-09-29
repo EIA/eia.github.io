8025cc23f6d643dc8f5555d82f0ccafdcae8841d
Eia mac 13 <angelliya00@gmail.com>
2020-09-29 16:10:40 +0800

    demo update

--- ironman/2021/demo/2020_09_28-2/lv2.js
+++ ironman/2021/demo/2020_09_28-2/lv2.js
@@ -5,9 +5,11 @@ document.body.appendChild(app.view);
 const resolution = PIXI.settings.RESOLUTION;
 const filterResolution = PIXI.settings.FILTER_RESOLUTION;
 const rendererResolution = app.renderer.resolution;
+const windowDevicePixelRatio = window.devicePixelRatio;
 
 alert(
     "resolution: " + resolution + "\n" + 
     "filterResolution: " + filterResolution + "\n" +
-    "rendererResolution: " + rendererResolution
+    "rendererResolution: " + rendererResolution + "\n" +
+    "window.devicePixelRatio: " + windowDevicePixelRatio
 );
