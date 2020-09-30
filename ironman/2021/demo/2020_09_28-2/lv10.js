fd222b280a6bb0cf9cf91e6c3e74799ffca628b7
Eia mac 13 <angelliya00@gmail.com>
2020-09-30 19:46:18 +0800

    demo update

---
+++ ironman/2021/demo/2020_09_28-2/lv10.js
@@ -0,0 +1,47 @@
+const app = new PIXI.Application({
+    width: 300,
+    height: 300,
+    backgroundColor: 0x1099bb,
+    resolution: window.devicePixelRatio,
+    autoResize: true
+});
+document.getElementById("pixiContainer").appendChild(app.view);
+
+
+const resolution = PIXI.settings.RESOLUTION;
+const filterResolution = PIXI.settings.FILTER_RESOLUTION;
+const rendererResolution = app.renderer.resolution;
+
+console.log('resolution: ', resolution);
+console.log('filterResolution: ', filterResolution);
+console.log('rendererResolution: ', rendererResolution);
+
+document.getElementById("resolution").innerText = "resolution: " + resolution;
+document.getElementById("filterResolution").innerText = "filterResolution: " + filterResolution;
+document.getElementById("rendererResolution").innerText = "rendererResolution: " + rendererResolution;
+
+const testPic = PIXI.Sprite.from('test_200x200.png');
+
+testPic.anchor.set(0.5);
+testPic.x = app.screen.width / 2;
+testPic.y = app.screen.height / 2;
+testPic.scale.x = testPic.scale.y = 0.5;
+
+app.stage.addChild(testPic);
+
+/*
+window.onresize = function (event) {
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+
+    app.view.style.width = w + "px";
+    app.view.style.height = h + "px";
+    app.renderer.resize(w, h);
+
+    testPic.x = app.screen.width / 2;
+    testPic.y = app.screen.height / 2;
+
+};
+
+onresize();
+*/
