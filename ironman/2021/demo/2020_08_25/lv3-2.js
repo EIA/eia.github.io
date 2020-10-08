840834df5b10b1162f6fcf098383108d81d38f2a
Eia mac 13 <angelliya00@gmail.com>
2020-10-08 23:58:43 +0800

    demo update

---
+++ ironman/2021/demo/2020_08_25/lv3-2.js
@@ -0,0 +1,17 @@
+const app = new PIXI.Application({ backgroundColor: 0x1099bb });
+document.body.appendChild(app.view);
+
+const createBox = ()=>{
+	const box = new PIXI.Graphics();
+	box.beginFill(0xff9900);
+	box.drawRect(-50, -50, 100, 100);
+	box.endFill();
+	return box;
+};
+
+
+const box1 = createBox();
+app.stage.addChild(box1);
+box1.x = app.screen.width / 2;
+box1.y = app.screen.height / 2;
+gsap.to(box1, {duration:3, y:app.screen.height / 2 - 100, yoyo: true, repeat:-1, ease:'easeIn'});
